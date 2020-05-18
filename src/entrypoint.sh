e11a1d479228819ff1f9b2e41ec210d1ed7f7e09
AndreasAugustin <dev@andreas-augustin.org>
2020-05-18 06:13:26 +0200

    feat(action): (#1) add script

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,5 +6,22 @@ set -e
     exit 1;
 };
 
+SOURCE_REPO=$1
 
-sh -c "/template-sync.sh $*"
+if [[ -z "$SOURCE_REPO" ]]; then
+  echo "Missing input 'source_repo: \${{ input.source_repo }}'.;"
+  exit 1
+fi
+
+UPSTREAM_REPO="${SOURCE_REPO}"
+SOURCE_BRANCH="master"
+NEW_BRANCH="chore/template_sync"
+
+git config --unset-all http."https://github.com/".extraheader
+git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+git remote add tmp_upstream "$UPSTREAM_REPO"
+git fetch tmp_upstream
+git remote --verbose
+git push origin "refs/remotes/tmp_upstream/${SOURCE_BRANCH}:refs/heads/${NEW_BRANCH}"
+git remote rm tmp_upstream
+git remote --verbose
