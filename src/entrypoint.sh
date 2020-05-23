351eca29f7ec72f47b7a437ea71796c1b8b81dae
AndreasAugustin <dev@andreas-augustin.org>
2020-05-23 17:55:17 +0200

    fix(): fix script -> environment variable no argument

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -15,11 +15,15 @@ UPSTREAM_REPO="${SOURCE_REPO}"
 SOURCE_BRANCH="master"
 NEW_BRANCH="chore/template_sync"
 
+echo "start sync"
 git config --unset-all http."https://github.com/".extraheader
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 git remote add tmp_upstream "$UPSTREAM_REPO"
+echo "fetching source repo ${UPSTREAM_REPO}"
 git fetch tmp_upstream
 git remote --verbose
+echo "push to new branch ${NEW_BRANCH}"
 git push origin "refs/remotes/tmp_upstream/${SOURCE_BRANCH}:refs/heads/${NEW_BRANCH}"
+echo "cleanup"
 git remote rm tmp_upstream
 git remote --verbose
