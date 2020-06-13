7ec755bc157be47d6a1c9c873b27ff924fa81d1e
andy Augustin <AndreasAugustin@users.noreply.github.com>
2020-06-13 17:12:04 +0200

    Fix/add ssh (#15)
    * fix(): add ssh agent to DEV docker container
    
    * fix(): change git pull strategy
    
    * fix(): change git pull strategy
    
    * fix(): switch github user name to GITHUB_ACTOR
    
    * fix(): switch github user name to GITHUB_ACTOR
    
    * fix(): switch from github-cli to hub
    
    * fix(): fix validation gate

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@
 FROM golang:1.14-alpine as DEV
 
 # install packages
-RUN apk add --update --no-cache bash make git zsh curl tmux musl build-base
+RUN apk add --update --no-cache bash make git zsh curl tmux musl build-base openssh
 
 # Make zsh your default shell for tmux
 RUN echo "set-option -g default-shell /bin/zsh" >> /root/.tmux.conf
@@ -12,10 +12,20 @@ RUN echo "set-option -g default-shell /bin/zsh" >> /root/.tmux.conf
 # install oh-my-zsh
 RUN sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-RUN git clone https://github.com/cli/cli.git gh-cli \
-    && cd gh-cli \
-    && make \
-    && mv ./bin/gh /usr/local/bin/
+# github-cli not stable yet
+#RUN git clone https://github.com/cli/cli.git gh-cli \
+#    && cd gh-cli \
+#    && make \
+#    && mv ./bin/gh /usr/local/bin/
+
+RUN apk add --update --no-cache groff util-linux
+RUN git clone \
+  --config transfer.fsckobjects=false \
+  --config receive.fsckobjects=false \
+  --config fetch.fsckobjects=false \
+  https://github.com/github/hub.git \
+ && cd hub \
+ && make install prefix=/usr/local
 
 WORKDIR /app
 
--- src/sync_template.sh
+++ src/sync_template.sh
@@ -6,8 +6,13 @@ if [[ -z "${SOURCE_REPO}" ]]; then
   exit 1;
 fi
 
-if ! [ -x "$(command -v gh)" ]; then
-  echo "Error: github-cli gh is not installed. 'https://github.com/cli/cli'" >&2;
+# if ! [ -x "$(command -v gh)" ]; then
+#   echo "Error: github-cli gh is not installed. 'https://github.com/cli/cli'" >&2;
+#   exit 1;
+# fi
+
+if ! [ -x "$(command -v hub)" ]; then
+  echo "Error: hub is not installed. 'https://github.com/github/hub'" >&2;
   exit 1;
 fi
 
@@ -17,10 +22,16 @@ echo "start sync"
 echo "create new branch from default branch with name ${NEW_BRANCH}"
 git checkout -b ${NEW_BRANCH}
 echo "pull changes from template"
-git pull "${SOURCE_REPO}" --allow-unrelated-histories
+git pull "${SOURCE_REPO}" --allow-unrelated-histories --squash --strategy=recursive -X theirs
 git add .
 git commit -m "chore(template): merge template changes :up:"
 echo "push changes"
 git push --set-upstream origin "${NEW_BRANCH}"
 echo "create pull request"
-gh pr create -b master -f -l chore
+# Workaround for `hub` auth error https://github.com/github/hub/issues/2149#issuecomment-513214342
+export GITHUB_USER="$GITHUB_ACTOR"
+hub pull-request \
+  -b master \
+  -h $NEW_BRANCH \
+  --no-edit
+# gh pr create -B master -f -l chore
