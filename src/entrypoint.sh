3ac8c7ffb4d69ba0db880ce5e5e24882de87e2d6
andy Augustin <AndreasAugustin@users.noreply.github.com>
2021-05-05 22:52:07 +0200

    Chore/log messages (#75)
    * chore(log): add meaningful log messages and labels
    
    * doc(): add information how to enable debug logs :rocket:
    
    * fix(): :hack: unset -u
    
    * fix(): unset -u (#65)

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,20 +1,25 @@
 #! /usr/bin/env bash
 set -e
+# set -u
+set -x
 
 [ -z "${GITHUB_TOKEN}" ] && {
-    echo "Missing input 'github_token: \${{ secrets.GITHUB_TOKEN }}'.";
+    echo "::error::Missing input 'github_token: \${{ secrets.GITHUB_TOKEN }}'.";
     exit 1;
 };
 
 if [[ -z "${SOURCE_REPO_PATH}" ]]; then
-  echo "Missing input 'source_repo_path: \${{ input.source_repo_path }}'.;"
+  echo "::error::Missing input 'source_repo_path: \${{ input.source_repo_path }}'.;"
   exit 1
 fi
 
-SOURCE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${SOURCE_REPO_PATH}"
+export SOURCE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${SOURCE_REPO_PATH}"
 
+echo "::group::git init"
 echo "set git global configuration"
-git config --global user.email "github-action@actions-template-sync.noreply.github.com@"
+git config --global user.email "github-action@actions-template-sync.noreply.github.com"
 git config --global user.name "${GITHUB_ACTOR}"
+echo "::endgroup::"
 
+# shellcheck source=sync_template.sh
 source sync_template.sh
