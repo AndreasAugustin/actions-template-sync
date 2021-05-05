831c066c4833665bff48f7bfbaeec7d42606dd6d
AndreasAugustin <dev@andreas-augustin.org>
2021-05-05 22:23:32 +0200

    chore(log): add meaningful log messages and labels

--- src/sync_template.sh
+++ src/sync_template.sh
@@ -1,57 +1,67 @@
 #! /usr/bin/env bash
 set -e
+set -u
+set -x
 
 if [[ -z "${SOURCE_REPO}" ]]; then
-  echo "Error: Missing env variable 'SOURCE_REPO'" >&2;
+  echo "::error::Missing env variable 'SOURCE_REPO'" >&2;
   exit 1;
 fi
 
 if [[ -z "${UPSTREAM_BRANCH}" ]]; then
-  echo "Error: Missing env variable 'UPSTREAM_BRANCH'" >&2;
+  echo "::error::Missing env variable 'UPSTREAM_BRANCH'" >&2;
   exit 1;
 fi
 
 if ! [ -x "$(command -v gh)" ]; then
-  echo "Error: github-cli gh is not installed. 'https://github.com/cli/cli'" >&2;
+  echo "::error::github-cli gh is not installed. 'https://github.com/cli/cli'" >&2;
   exit 1;
 fi
 
 TEMPLATE_VERSION_FILE_NAME=".templateversionrc"
-TEMPLATE_REMOTE_GIT_HASH=$(git ls-remote "${SOURCE_REPO}" HEAD | awk {'print $1}')
+TEMPLATE_REMOTE_GIT_HASH=$(git ls-remote "${SOURCE_REPO}" HEAD | awk '{print $1}')
 NEW_TEMPLATE_GIT_HASH=$(git rev-parse --short "${TEMPLATE_REMOTE_GIT_HASH}")
 NEW_BRANCH="chore/template_sync_${NEW_TEMPLATE_GIT_HASH}"
 
-echo "start sync"
-echo "create new branch from default branch with name ${NEW_BRANCH}"
-git checkout -b "${NEW_BRANCH}"
-echo "pull changes from template"
-git pull "${SOURCE_REPO}" --allow-unrelated-histories --squash --strategy=recursive -X theirs
-
-echo "new Git HASH ${NEW_TEMPLATE_GIT_HASH}"
+echo "::group::Check new changes"
+echo "::debug::new Git HASH ${NEW_TEMPLATE_GIT_HASH}"
 if [ -r ${TEMPLATE_VERSION_FILE_NAME} ]
 then
   CURRENT_TEMPLATE_GIT_HASH=$(cat ${TEMPLATE_VERSION_FILE_NAME})
-  echo "Current git hash ${CURRENT_TEMPLATE_GIT_HASH}"
+  echo "::debug::Current git hash ${CURRENT_TEMPLATE_GIT_HASH}"
 fi
 
 if [ "${NEW_TEMPLATE_GIT_HASH}" == "${CURRENT_TEMPLATE_GIT_HASH}" ]
 then
-  echo "repository is up to date"
+  echo "::warn::repository is up to date"
   exit 0
 fi
+echo "::endgroup::"
 
+echo "::group::Pull template"
+echo "::debug::create new branch from default branch with name ${NEW_BRANCH}"
+git checkout -b "${NEW_BRANCH}"
+echo "::debug::pull changes from template"
+git pull "${SOURCE_REPO}" --allow-unrelated-histories --squash --strategy=recursive -X theirs
+echo "::endgroup::"
+
+echo "::group::persist template version"
 echo "write new template version file"
 echo "${NEW_TEMPLATE_GIT_HASH}" > ${TEMPLATE_VERSION_FILE_NAME}
-echo "wrote new template version file with content $(cat ${TEMPLATE_VERSION_FILE_NAME})"
+echo "::debug::wrote new template version file with content $(cat ${TEMPLATE_VERSION_FILE_NAME})"
+echo "::endgroup::"
 
+echo "::group::commit and push changes"
 git add .
 git commit -m "chore(template): merge template changes :up:"
 
-echo "push changes"
+echo "::debug::push changes"
 git push --set-upstream origin "${NEW_BRANCH}"
-echo "create pull request"
+echo "::endgroup::"
 
+echo "::group::create pull request"
 gh pr create \
   --title "upstream merge template repository" \
   --body "Merge ${SOURCE_REPO_PATH} ${NEW_TEMPLATE_GIT_HASH}" \
   -B "${UPSTREAM_BRANCH}"
+echo "::endgroup::"
