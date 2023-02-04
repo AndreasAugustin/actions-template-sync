1eff6e38222c71b439559d5c668a43c391d51c68
Andy Augustin <dev@andreas-augustin.org>
2023-02-04 13:57:36 +0100

    feat: :sparkles: add hooks (#220)

--- src/sync_common.sh
+++ src/sync_common.sh
@@ -39,3 +39,29 @@ warn() {
 info() {
   echo "::info::$*";
 }
+
+#######################################
+# Executes commands defined within yml file
+# Arguments:
+#   hook -> the hook to use
+#
+####################################3#
+cmd_from_yml_file() {
+  local FILE_NAME="templatesync.yml"
+  local HOOK=$1
+  local YML_PATH=".hooks.${HOOK}.commands"
+
+  if [ "$IS_ALLOW_HOOKS" != "true" ]; then
+    debug "execute cmd hooks not enabled"
+  else
+    info "execute cmd hooks enabled"
+
+    if ! [ -x "$(command -v yq)" ]; then
+      err "yaml query yq is not installed. 'https://mikefarah.gitbook.io/yq/'";
+      exit 1;
+    fi
+    readarray cmd_Arr < <(yq "${YML_PATH} | .[]"  "${FILE_NAME}")
+
+    for key in "${cmd_Arr[@]}"; do printf '%s\n' "$(${key})"; done
+  fi
+}
