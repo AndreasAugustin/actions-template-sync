a247a600b0d5d62e35c7dec91ca445f343771470
Andy Augustin <dev@andreas-augustin.org>
2023-02-12 15:51:58 +0100

    feat :rocket: add test for gitlab repo (#278)

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -58,8 +58,13 @@ function git_init() {
   git config --global --add safe.directory /github/workspace
   git lfs install
 
-  gh auth setup-git --hostname "${SOURCE_REPO_HOSTNAME}"
-  gh auth status --hostname "${SOURCE_REPO_HOSTNAME}"
+  if [[ -n "${IS_NOT_SOURCE_GITHUB}" ]]; then
+    info "the source repository is not located within GitHub."
+  else
+    info "the source repository is located within GitHub."
+    gh auth setup-git --hostname "${SOURCE_REPO_HOSTNAME}"
+    gh auth status --hostname "${SOURCE_REPO_HOSTNAME}"
+  fi
   echo "::endgroup::"
 }
 
