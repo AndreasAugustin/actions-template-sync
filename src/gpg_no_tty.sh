f57d687bd2ee51e73e17d6d94e930571a06a391f
Andy Augustin <dev@andreas-augustin.org>
2024-01-07 21:28:21 +0100

    fix: :bug: (#204) fix issue with no passphrase

--- src/gpg_no_tty.sh
+++ src/gpg_no_tty.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-/usr/bin/gpg --batch --no-tty "$@"
+
+export pflag=""
+# create dynamic `--passphrase` flag to insert into the final command if the passphrase variable is not empty.
+if [[ -n "$GPG_PASSPHRASE" ]];  then
+    pflag="--passphrase ${GPG_PASSPHRASE}"
+fi
+
+# "<&0" → use same stdin as the one originally piped to script
+# "$@" → pass all script arguments to actual command
+
+gpg --yes --batch --no-tty "$pflag" "$@" <&0
+
+exit $?
