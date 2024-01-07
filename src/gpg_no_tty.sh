d4dadf09c705e6d90b6be606ab0e6bcc17619afc
Andy Augustin <dev@andreas-augustin.org>
2024-01-07 21:58:17 +0100

    fix: :bug: (#204) fix

--- src/gpg_no_tty.sh
+++ src/gpg_no_tty.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -n "${GPG_PASSPHRASE}" ]]  &>/dev/null;  then
-    echo -e "${GPG_PASSPHRASE}" |  gpg --batch --yes --passphrase-fd 0 --no-tty "$@" <&0
+    echo -e "${GPG_PASSPHRASE}" |  gpg --no-tty --batch --yes --passphrase-fd 0 "$@" <&0
 else
   gpg --yes --batch --no-tty "$@" <&0
 fi
