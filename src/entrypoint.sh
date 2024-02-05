3b9a8396884d7aea969c4af22379837ccc5caff8
Andy Augustin <dev@andreas-augustin.org>
2024-02-05 21:10:20 +0100

    fix: :bug: trial for gpg sign

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -44,8 +44,10 @@ function ssh_setup() {
 function gpg_setup() {
   echo "::group::gpg setup"
   info "start prepare gpg"
+  GPG_TTY=$(tty)
+  export GPG_TTY
   echo -e "$GPG_PRIVATE_KEY" | gpg --import --batch
-  # for fpr in $(gpg --list-key --with-colons "${GIT_USER_EMAIL}"  | awk -F: '/fpr:/ {print $10}' | sort -u); do  echo -e "5\ny\n" |  gpg --no-tty --command-fd 0 --expert --edit-key "$fpr" trust; done
+  for fpr in $(gpg --list-key --with-colons "${GIT_USER_EMAIL}"  | awk -F: '/fpr:/ {print $10}' | sort -u); do  echo -e "5\ny\n" |  gpg --no-tty --command-fd 0 --expert --edit-key "$fpr" trust; done
 
   KEY_ID="$(gpg --list-secret-key --with-colons "${GIT_USER_EMAIL}" | awk -F: '/sec:/ {print $5}')"
   git config --global user.signingkey "${KEY_ID}"
--- src/gpg_no_tty.sh
+++ src/gpg_no_tty.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ -n "${GPG_PASSPHRASE}" ]]  &>/dev/null;  then
-    echo -e "${GPG_PASSPHRASE}" |  gpg --pinentry-mode loopback --batch --yes --passphrase-fd 0 "$@" <&0
+    # echo -e "${GPG_PASSPHRASE}" |  gpg --pinentry-mode loopback --batch --yes --passphrase-fd 0 "$@" <&0
+    echo -e "${GPG_PASSPHRASE}" |  gpg --batch --yes --passphrase-fd 0 "$@" <&0
 else
   gpg --pinentry-mode loopback --yes --batch "$@" <&0
 fi
