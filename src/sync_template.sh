ab4153bb937a2d7ad996f0693977cb943443e8b3
andy Augustin <AndreasAugustin@users.noreply.github.com>
2024-03-02 11:41:03 +0100

    fix(#476): :bug: output variable edge cases (#479)
    * fix(#476): :bug: issue with empty output variable
    
    * fix: :bug:

--- src/sync_template.sh
+++ src/sync_template.sh
@@ -48,11 +48,24 @@ debug "new Git HASH ${NEW_TEMPLATE_GIT_HASH}"
 
 echo "::group::Check new changes"
 
+function set_github_action_outputs() {
+  echo "::group::set gh action outputs"
+  if [[ -z "${GITHUB_RUN_ID}" ]]; then
+    info "env var 'GITHUB_RUN_ID' is empty -> no github action workflow"
+  else
+    # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
+    echo "pr_branch=${NEW_BRANCH}" >> "$GITHUB_OUTPUT"
+  fi
+  echo "::endgroup::"
+}
+
+
 function check_branch_remote_existing() {
   git ls-remote --exit-code --heads origin "${NEW_BRANCH}" || BRANCH_DOES_NOT_EXIST=true
 
   if [[ "${BRANCH_DOES_NOT_EXIST}" != true ]]; then
     warn "Git branch '${NEW_BRANCH}' exists in the remote repository"
+    set_github_action_outputs
     exit 0
   fi
 }
@@ -239,16 +252,5 @@ else
 fi
 
 echo "::endgroup::"
-
-function set_github_action_outputs() {
-  echo "::group::set gh action outputs"
-  if [[ -z "${GITHUB_RUN_ID}" ]]; then
-    info "env var 'GITHUB_RUN_ID' is empty -> no github action workflow"
-  else
-    # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
-    echo "pr_branch=${NEW_BRANCH}" >> "$GITHUB_OUTPUT"
-  fi
-  echo "::endgroup::"
-}
 
 set_github_action_outputs
