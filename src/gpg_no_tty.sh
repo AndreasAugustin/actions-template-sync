b7303012557d1a281249447d7d02ece4746e14ce
Andy Augustin <dev@andreas-augustin.org>
2024-03-11 20:37:28 +0100

    fix: :bug:

--- src/gpg_no_tty.sh
+++ src/gpg_no_tty.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -n "${GPG_PASSPHRASE}" ]]  &>/dev/null;  then
-    # FIXME(anau) the next line is a bug
-    echo -e "${GPG_PASSPHRASE}" |  gpg --pinentry-mode loopback --batch --yes --passphrase-fd 0 "$@" <&0
+    gpg --pinentry-mode loopback --batch --yes --passphrase "${GPG_PASSPHRASE}" "$@" <&0
 else
   gpg --pinentry-mode loopback --yes --batch "$@" <&0
 fi
