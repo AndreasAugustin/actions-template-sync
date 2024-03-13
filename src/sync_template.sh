979090e8613d7411268e0cac3ac625d98c79ff10
Andy Augustin <dev@andreas-augustin.org>
2024-03-13 22:44:34 +0100

    style: :rotating_light: fix lint issues
    Signed-off-by: Andy Augustin <dev@andreas-augustin.org>

--- src/sync_template.sh
+++ src/sync_template.sh
@@ -6,7 +6,7 @@ set -e
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-# shellcheck source=./sync_template.sh
+# shellcheck source=src/sync_template.sh
 source "${SCRIPT_DIR}/sync_common.sh"
 
 ############################################
