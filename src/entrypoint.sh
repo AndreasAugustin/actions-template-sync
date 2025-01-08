0a182963a42ebad9e7645173326c0138835a88ce
Andy Augustin <dev@andreas-augustin.org>
2025-01-08 23:19:32 +0100

    feat(#586): :sparkles: add option to sync within multiple orgs
    Signed-off-by: Andy Augustin <dev@andreas-augustin.org>

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -12,8 +12,8 @@ source "${SCRIPT_DIR}/sync_common.sh"
 # Precheks
 ##########################################
 
-if [[ -z "${ORG_GITHUB_TOKEN}" ]]; then
-    err "Missing input 'org_github_token: \${{ secrets.ORG_GITHUB_TOKEN }}'.";
+if [[ -z "${SOURCE_REPO_GITHUB_TOKEN}" ]]; then
+    err "Missing input 'github_token: \${{ secrets.GITHUB_TOKEN }}'.";
     exit 1;
 fi
 
@@ -37,7 +37,7 @@ fi
 ############################################
 
 DEFAULT_REPO_HOSTNAME="github.com"
-SOURCE_REPO_HOSTNAME="${HOSTNAME:-${DEFAULT_REPO_HOSTNAME}}"
+export SOURCE_REPO_HOSTNAME="${HOSTNAME:-${DEFAULT_REPO_HOSTNAME}}"
 GIT_USER_NAME="${GIT_USER_NAME:-${GITHUB_ACTOR}}"
 GIT_USER_EMAIL="${GIT_USER_EMAIL:-github-action@actions-template-sync.noreply.${SOURCE_REPO_HOSTNAME}}"
 
@@ -175,7 +175,7 @@ function git_init() {
 if [[ -n "${SSH_PRIVATE_KEY_SRC}" ]] &>/dev/null; then
   ssh_setup "${SSH_PRIVATE_KEY_SRC}" "${SOURCE_REPO_HOSTNAME}"
 elif [[ "${SOURCE_REPO_HOSTNAME}" != "${DEFAULT_REPO_HOSTNAME}" ]]; then
-  gh auth login --git-protocol "https" --hostname "${SOURCE_REPO_HOSTNAME}" --with-token <<< "${ORG_GITHUB_TOKEN}"
+  gh auth login --git-protocol "https" --hostname "${SOURCE_REPO_HOSTNAME}" --with-token <<< "${SOURCE_REPO_GITHUB_TOKEN}"
 fi
 
 export SOURCE_REPO="${SOURCE_REPO_PREFIX}${SOURCE_REPO_PATH}"
