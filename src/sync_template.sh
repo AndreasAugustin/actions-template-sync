542986e75c6747c11d22c380bb34f11e536ac1f2
Alex Esseling <alexander.esseling@gmail.com>
2025-01-10 23:43:34 +0100

    fix: simplify logout command in sync_template.sh for source repository

--- src/sync_template.sh
+++ src/sync_template.sh
@@ -256,9 +256,8 @@ function pull_source_changes() {
 
   if [[ -n "${SRC_SSH_PRIVATEKEY_ABS_PATH}" ]] &>/dev/null; then
     info "we are using ssh for the source repo. No need to logout."
-  else 
-    
-    gh auth logout --hostname "${SOURCE_REPO_HOSTNAME}"
+  else     
+    gh auth logout 
     info "logging in into the target with hostname ${HOSTNAME}"
     gh auth login --git-protocol "https" --hostname "${HOSTNAME}" --with-token <<< "${GH_TOKEN}"
     info "logged in"
