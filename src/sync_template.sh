3d9bd0e4a9bd68281a5a88af80545615a522ba16
Alex Esseling <alexander.esseling@gmail.com>
2025-01-10 15:56:28 +0100

    fix: update logout command to use GITHUB_SERVER_URL in sync_template.sh

--- src/sync_template.sh
+++ src/sync_template.sh
@@ -257,7 +257,7 @@ function pull_source_changes() {
   if [[ -n "${SRC_SSH_PRIVATEKEY_ABS_PATH}" ]] &>/dev/null; then
     info "we are using ssh for the source repo. No need to logout."
   else
-    gh auth logout --hostname "${SOURCE_REPO_HOSTNAME}"
+    gh auth logout --hostname "${GITHUB_SERVER_URL}"
 
     info "logging in into the target with hostname ${GITHUB_SERVER_URL}"
     gh auth login --git-protocol "https" --hostname "${GITHUB_SERVER_URL}"
