bbe7b31e9ec99ea94c4035ae411b497ba39ea646
Alex Esseling <alexander.esseling@gmail.com>
2025-01-13 10:52:57 +0100

    fix: change log message for missing TARGET_GH_TOKEN and improve authentication handling in git_init function

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -17,7 +17,7 @@ if [[ -z "${SOURCE_GH_TOKEN}" ]]; then
 fi
 
 if [[ -z "${TARGET_GH_TOKEN}" ]]; then
-   info "Missing input for TARGET_GH_TOKEN. Will use \${{ secrets.GITHUB_TOKEN }}'.";
+   err "Missing input for TARGET_GH_TOKEN. Will use \${{ secrets.GITHUB_TOKEN }}'.";
 fi
 
 if [[ -z "${SOURCE_REPO_PATH}" ]]; then
@@ -164,8 +164,10 @@ function git_init() {
     ssh-keyscan -t rsa "${source_repo_hostname}" >> "${HOME}"/.ssh/known_hosts
   else
     info "the source repository is located within GitHub."               
-    gh auth login --git-protocol "https" --hostname "${SOURCE_REPO_HOSTNAME}" --with-token <<< "${SOURCE_GH_TOKEN}"
-    gh auth status --hostname "${source_repo_hostname}"
+    if [[ -n "${SOURCE_GH_TOKEN}" ]]; then
+      info "login to the source repository"
+      gh auth login --git-protocol "https" --hostname "${SOURCE_REPO_HOSTNAME}" --with-token <<< "${SOURCE_GH_TOKEN}"
+    fi    
     info "login to the target repository"
     gh auth login --git-protocol "https" --hostname "${SOURCE_REPO_HOSTNAME}" --with-token <<< "${TARGET_GH_TOKEN}"
     gh auth status --hostname "${source_repo_hostname}"
