225b28efd1ffdc852d6170385fdf127172799128
Alex Esseling <alexander.esseling@gmail.com>
2025-01-20 11:33:30 +0100

    fix: remove redundant unset of GITHUB_TOKEN before logging into the source repository in entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -165,8 +165,7 @@ function git_init() {
   else
     info "the source repository is located within GitHub."               
     if [[ -n "${SOURCE_GH_TOKEN}" ]]; then
-      info "login to the source repository"
-      unset GITHUB_TOKEN
+      info "login to the source repository"      
       gh auth login --git-protocol "https" --hostname "${SOURCE_REPO_HOSTNAME}" --with-token <<< "${SOURCE_GH_TOKEN}"
       info "login to the target repository"
       gh auth login --git-protocol "https" --hostname "${SOURCE_REPO_HOSTNAME}" --with-token <<< "${GITHUB_TOKEN}"
