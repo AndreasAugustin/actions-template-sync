8af40d319f224d07b6568503a4e91d9ca9076ed6
Alex Esseling <alexander.esseling@gmail.com>
2025-01-20 14:26:26 +0100

    fix: simplify authentication command in entrypoint.sh by removing token input

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -170,7 +170,7 @@ function git_init() {
       info "done set git global configuration"    
     else
       if [[ -n "${GITHUB_TOKEN}" ]]; then
-        gh auth login --git-protocol "https" --hostname "${SOURCE_REPO_HOSTNAME}" --with-token <<< "${GITHUB_TOKEN}"
+        gh auth login --git-protocol "https" --hostname "${SOURCE_REPO_HOSTNAME}" 
       fi
       gh auth setup-git --hostname "${source_repo_hostname}"
       gh auth status --hostname "${source_repo_hostname}"
